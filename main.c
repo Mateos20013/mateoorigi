956b59fd49108ccd02847f840758c4d01d0ded77
Mateos20013 <mateo.sotomayor@udla.edu.ec>
2023-04-14 00:06:01 -0500

    inicio

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 int main()
 {
@@ -9,8 +10,10 @@ int main()
     float total = 0;
     int cantidad;
     char respuesta;
-    char Nombre[50];
-    char cedula[11];
+    char nombre[50];
+    char cedula[50];
+    char detallesFactura[1500] = "";
+    char charValue[200];
 
     do
     {
@@ -24,51 +27,64 @@ int main()
         switch (opcion)
         {
         case 1:
-            item = 0;
-
-            printf("\n--item--\n");
-            printf("1. Llantas (Precio: $150)\n");
-            printf("2. Kit Pastill  as de freno (Precio: $55)\n");
-            printf("3. Kit de embrague (Precio: $180)\n");
-            printf("4. Faro (Precio: $70)\n");
-            printf("5. Radiador (Precio: $120)\n");
-            printf("Seleccione un item: ");
-            scanf("%d", &item);
-
-            switch (item)
+            do
             {
-            case 1:
-                printf("Ingrese la cantidad de llantas: ");
-                scanf("%d", &cantidad);
-                subtotal += cantidad * 150;
-                break;
-            case 2:
-                printf("Ingrese la cantidad de kits de pastillas de freno: ");
-                scanf("%d", &cantidad);
-                subtotal += cantidad * 55;
-                break;
-            case 3:
-                printf("Ingrese la cantidad de kits de embrague: ");
-                scanf("%d", &cantidad);
-                subtotal += cantidad * 180;
-                break;
-            case 4:
-                printf("Ingrese la cantidad de faros: ");
-                scanf("%d", &cantidad);
-                subtotal += cantidad * 70;
-                break;
-            case 5:
-                printf("Ingrese la cantidad de radiadores: ");
-                scanf("%d", &cantidad);
-                subtotal += cantidad * 120;
-                break;
-            default:
-                printf("Opción inválida\n");
-                break;
-            }
+                item = 0;
 
-            printf("¿Desea facturar otro producto? (S/N): ");
-            scanf(" %c", &respuesta);
+                printf("\n--item--\n");
+                printf("1. Llantas (Precio: $150)\n");
+                printf("2. Kit Pastillas de freno (Precio: $55)\n");
+                printf("3. Kit de embrague (Precio: $180)\n");
+                printf("4. Faro (Precio: $70)\n");
+                printf("5. Radiador (Precio: $120)\n");
+                printf("Seleccione un item: ");
+                scanf("%d", &item);
+
+                switch (item)
+                {
+                case 1:
+                    printf("Ingrese la cantidad de llantas: ");
+                    scanf("%d", &cantidad);
+                    subtotal += cantidad * 150.0;
+                    sprintf(charValue, "Llantas\t\t\t\t%d\t\t%.2f\n", cantidad, cantidad * 150.0);
+                    strcat(detallesFactura, charValue);
+                    break;
+                case 2:
+                    printf("Ingrese la cantidad de kits de pastillas de freno: ");
+                    scanf("%d", &cantidad);
+                    subtotal += cantidad * 55.0;
+                    sprintf(charValue, "Kit Pastillas de freno\t\t%d\t\t%.2f\n", cantidad, cantidad * 55.0);
+                    strcat(detallesFactura, charValue);
+                    break;
+                case 3:
+                    printf("Ingrese la cantidad de kits de embrague: ");
+                    scanf("%d", &cantidad);
+                    subtotal += cantidad * 180;
+                    sprintf(charValue, "Faro\t\t\t\t%d\t\t%.2f\n", cantidad, cantidad * 180.0);
+                    strcat(detallesFactura, charValue);
+                    break;
+                case 4:
+                    printf("Ingrese la cantidad de faros: ");
+                    scanf("%d", &cantidad);
+                    subtotal += cantidad * 70;
+                    sprintf(charValue, "Faro\t\t\t\t%d\t\t%.2f\n", cantidad, cantidad * 70.0);
+                    strcat(detallesFactura, charValue);
+                    break;
+                case 5:
+                    printf("Ingrese la cantidad de radiadores: ");
+                    scanf("%d", &cantidad);
+                    subtotal += cantidad * 120;
+                    sprintf(charValue, "Radiador\t\t\t%d\t\t%.2f\n", cantidad, cantidad * 120.0);
+                    strcat(detallesFactura, charValue);
+                    break;
+                default:
+                    printf("Opción inválida\n");
+                    break;
+                }
+
+                printf("¿Desea facturar otro producto? (S/N): ");
+                scanf(" %c", &respuesta);
+            } while (respuesta == 'S' || respuesta == 's');
             break;
         case 2:
             if (subtotal == 0)
@@ -79,18 +95,14 @@ int main()
             {
                 printf("\n--Factura--\n");
                 printf("Nombre del comprador: ");
-                scanf("%s", Nombre);
+                scanf("%s", nombre);
                 printf("Número de cédula del comprador: ");
                 scanf("%s", cedula);
 
-                printf("Producto\tCantidad\tPrecio\n");
-                printf("--------------------------------------\n");
-                printf("Llantas\t\t%d\t\t$%.2f\n", (int)(subtotal / 150), (int)(subtotal / 150) * 150.0);
-                printf("Kit Pastillas de freno\t%d\t\t$%.2f\n", (int)(subtotal / 55), (int)(subtotal / 55) * 55.0);
-                printf("Kit de embrague\t\t%d\t\t$%.2f\n", (int)(subtotal / 180), (int)(subtotal / 180) * 180.0);
-                printf("Faro\t\t%d\t\t$%.2f\n", (int)(subtotal / 70), (int)(subtotal / 70) * 70.0);
-                printf("Radiador\t%d\t\t$%.2f\n", (int)(subtotal / 120), (int)(subtotal / 120) * 120.0);
-                printf("--------------------------------------\n");
+                printf("Producto\t\t\tCantidad\tPrecio\n");
+                printf("------------------------------------------------------\n");
+                printf("%s", detallesFactura);
+                printf("-------------------------------------------------------\n");
                 if (subtotal <= 500)
                 {
                     descuento = 0;
@@ -110,9 +122,9 @@ int main()
 
                 total = subtotal - descuento;
 
-                printf("Subtotal\t\t\t\t$%.2f\n", subtotal);
-                printf("Descuento (%.2f%%)\t\t\t$%.2f\n", descuento / subtotal * 100, descuento);
-                printf("Total\t\t\t\t\t$%.2f\n", total);
+                printf("Subtotal\t\t\t\t\t$%.2f\n", subtotal);
+                printf("Descuento (%.2f%%)\t\t\t\t$%.2f\n", descuento / subtotal * 100, descuento);
+                printf("Total\t\t\t\t\t\t$%.2f\n", total);
             }
 
 
@@ -121,7 +133,11 @@ int main()
             getchar(); // para esperar a que el usuario presione una tecla
             break;
         case 3:
-            printf("Saliendo del programa...Opción inválida\n");
+            printf("Saliendo del programa...\n");
+            break;
+        default:
+            printf("Opción inválida\n");
+            break;
         }
 
     } while (opcion != 3);
